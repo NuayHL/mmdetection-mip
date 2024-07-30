9512444b296ccf2ae9b2727417e0acbfc8d4f488
NuayHL <liuhy.df.xin@gmail.com>
2024-07-30 20:22:42 +0900

    add dino for train

--- model_test.py
+++ model_test.py
@@ -1,17 +1,18 @@
 import torch
-# import mmdet.models.data_preprocessors
+import mmdet.models.data_preprocessors
 from mmengine.config import Config
 from mmdet.registry import MODELS
 from mmdet.utils import register_all_modules
 register_all_modules()
 
-cfg = Config.fromfile('configs/yolox/yolox_s_test1_mip_4xb16-300e_voc.py')
+cfg = Config.fromfile('configs/yolox/yolox_s_test3_x3_1xb32_voc.py')
 
 detector = MODELS.build(cfg.model).cuda()
 
 input_tensor = torch.randn((3, 3, 640, 640)).cuda()
 
-predict = detector(input_tensor, mode='predict')
+predict = detector(input_tensor, mode='tensor')
 
 print(predict)
+
 
