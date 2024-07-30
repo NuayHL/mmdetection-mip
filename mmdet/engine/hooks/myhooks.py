9c22854ffddfa54a7022199b6e4803e5a3fb759a
NuayHL <liuhy.df.xin@gmail.com>
2024-07-30 15:18:39 +0900

    fix temp change in dist train

--- mmdet/engine/hooks/myhooks.py
+++ mmdet/engine/hooks/myhooks.py
@@ -15,6 +15,7 @@ class ChangeTempHook(Hook):
         self.init_temp = 1.0
         self.fin_temp = fin_temp
         self.current_epoch = 0
+        self.is_dist = False
 
     @property
     def increase_interval(self):
@@ -26,16 +27,23 @@ class ChangeTempHook(Hook):
 
     def before_train(self, runner) -> None:
         self.max_epoch = runner.max_epochs
-        assert hasattr(runner.model.bbox_head, 'temperature'), (
+        model = runner.model
+        if is_model_wrapper(runner.model):
+            model = model.module
+            self.is_dist = True
+        assert hasattr(model, 'temperature'), (
             runner.logger.error(f'The module {runner.model_name}.bbox_head has no attribute \'temperature\'. '
                                 f'Considering delete \'ChangeTempHook\' hook in config'))
-        self.init_temp = runner.model.bbox_head.temperature
+        self.init_temp = model.bbox_head.temperature
         runner.logger.info(f'Apply dynamic temperature. Init temp={self.init_temp}, Fin temp={self.fin_temp}')
 
     def before_train_epoch(self, runner) -> None:
         self.current_epoch = runner.epoch
         _progress = 0.5 * (1 - math.cos((self.step + 1) * math.pi / self.max_epoch))
-        runner.model.bbox_head.temperature = _progress * self.increase_interval + self.init_temp
+        if self.is_dist:
+            runner.model.module.bbox_head.temperature = _progress * self.increase_interval + self.init_temp
+        else:
+            runner.model.bbox_head.temperature = _progress * self.increase_interval + self.init_temp
         runner.logger.info(f'Epoch[{self.step}/{self.max_epoch}], model temp change to {runner.model.bbox_head.temperature}')
 
 
