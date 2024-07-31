f88fc4d651aabaa4768ea18f7267d500c00824a4
NuayHL <liuhy.df.xin@gmail.com>
2024-07-31 19:24:25 +0900

    add dino for train on supermicro

--- configs/dino/dino-4scale_r50_4xb4-12e_coco.py
+++ configs/dino/dino-4scale_r50_4xb4-12e_coco.py
@@ -125,14 +125,14 @@ train_pipeline = [
     dict(type='PackDetInputs')
 ]
 train_dataloader = dict(
-    batch_size=8,
-    num_workers=8,
+    batch_size=4,
+    num_workers=4,
     dataset=dict(
         filter_cfg=dict(filter_empty_gt=False), pipeline=train_pipeline))
 
 val_dataloader = dict(
     batch_size=2,
-    num_workers=8,)
+    num_workers=4,)
 
 test_dataloader = val_dataloader
 
